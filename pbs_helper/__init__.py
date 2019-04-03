56f49a04f7fabbc0783f5d1c89c5e30c1cabb0ff
ywaby <ywaby@debian.ywaby@163.com>
2019-04-03 18:35:06 +0800

    add licene

--- pbs_helper/__init__.py
+++ pbs_helper/__init__.py
@@ -1,15 +1,18 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
+# Copyright (C) 2019 ywabygl@gmail.com
+# 
+# PBS Helper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# 
+# PBS Helper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PBS Helper. If not, see <http://www.gnu.org/licenses/>.
+
 
 bl_info = {
     "name" : "PBS Helper",
@@ -60,9 +63,6 @@ class UI(bpy.types.Panel):
         col = layout.col
         col = layout.col
 
-
-
-
 class auto_rotate_light(bpy.types.Operator):
     '''preivew light rotate around active object'''
     pass
@@ -77,7 +77,7 @@ def init_addon():
 
 class PBS_Helper(bpy.types.AddonPreferences):
     bl_idname = __name__
-    sync_paint_node=BoolProperty(
+    sync_paint_node:BoolProperty(
         name='Sync Node Paint',
         default=True,
         description='Sync Active Shader Node With Paint Texture'
@@ -87,17 +87,15 @@ class PBS_Helper(bpy.types.AddonPreferences):
             col = layout.column()
             col.prop(self, "sync_paint_node")
 
-# append workspace PBR
-def register():
-    init_addon()
-    auto_load.register()
 
-def unregister():
-    auto_load.unregister()
+from .material_bake import BakeMaterial
+from .paint import Paint2Node
 
 classes = (
     BakeMaterial,
+    Paint2Node
 )
+
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
@@ -106,3 +104,4 @@ def register():
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
+
--- pbs_helper/paint.py
+++ pbs_helper/paint.py
@@ -1,4 +1,38 @@
+# Copyright (C) 2019 ywabygl@gmail.com
+# 
+# PBS Helper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# PBS Helper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with PBS Helper. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+helper for PBR paint workflow
+'''
+from bpy.types import (
+    Operator,
+    PropertyGroup,
+    AddonPreferences,
+    ShaderNodeGroup
+)
+from bpy.props import (
+    BoolProperty,
+    EnumProperty,
+    FloatProperty,
+    FloatVectorProperty,
+    IntProperty,
+    PointerProperty,
+    StringProperty,
+)
+
 # brush lib
-class Paint2Node():
+class Paint2Node(Operator):
     '''sync paint texture with active node'''
     pass
